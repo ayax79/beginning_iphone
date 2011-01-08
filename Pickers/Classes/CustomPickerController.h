cf2ed6e96a0aa2452d92ec892d7d4a891c1b28f5
A.J. Wright <ayax79@gmail.com>
2011-01-07 18:12:35 -0800

    pickers work

--- Pickers/Classes/CustomPickerController.h
+++ Pickers/Classes/CustomPickerController.h
@@ -9,8 +9,22 @@
 #import <UIKit/UIKit.h>
 
 
-@interface CustomPickerController : UIViewController {
-
+@interface CustomPickerController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate> {
+	UIPickerView *picker;
+	UILabel *winLabel;
+	
+	NSArray *column1;
+	NSArray *column2;
+	NSArray *column3;
+	NSArray *column4;
+	NSArray *column5;
 }
-
+@property(nonatomic, retain) IBOutlet UIPickerView *picker;
+@property(nonatomic, retain) IBOutlet UILabel *winLabel;
+@property(nonatomic, retain) NSArray *column1;
+@property(nonatomic, retain) NSArray *column2;
+@property(nonatomic, retain) NSArray *column3;
+@property(nonatomic, retain) NSArray *column4;
+@property(nonatomic, retain) NSArray *column5;
+-(IBAction)spin;
 @end
